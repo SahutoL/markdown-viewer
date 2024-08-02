712cea16713c26c3d56debf03d4e545cc6e2fac1
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-08-02 20:55:42 +0900

    service-worker.js

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -27,3 +27,18 @@ self.addEventListener('fetch', (event) => {
     )
   );
 });
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
